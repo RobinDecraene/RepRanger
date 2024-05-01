f99c0c0834b9a1223ed00da315994da931ac2b26
decraenerobin <robin@esign.eu>
2024-05-01 17:41:29 +0200

    Update: design

--- src/BottomTab/Account.js
+++ src/BottomTab/Account.js
@@ -1,11 +1,16 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import React from 'react';
+import { P } from '../Components/Text';
 
 const Account = () => {
   return (
     <View style={styles.container}>
-      <Text>Account</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.profileImg}
+        source={require('../../assets/images/no-img.png')}
+      />
+      <P>Voornaam Achternaam</P>
+
     </View>
   );
 }
@@ -15,8 +20,19 @@ export default Account
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  profileImg: {
+    height: null,
+    aspectRatio: 1,
+    width: '60%',
+    borderRadius: 120,
+    marginTop: 30,
+    marginBottom: 20,
   },
 });
