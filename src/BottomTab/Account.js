3d42cddf0daf998de3d64d800f14582a7faf598e
decraenerobin <robin@esign.eu>
2024-05-02 14:00:39 +0200

    Add: display current user name

--- src/BottomTab/Account.js
+++ src/BottomTab/Account.js
@@ -1,21 +1,56 @@
 import { Image, StyleSheet, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { firebase } from '../../Firebase';
 import { P } from '../../Components/Text';
+import { Button } from '../../Components/Button';
+import { useNavigation } from '@react-navigation/native';
 
 const Account = () => {
+  const navigation = useNavigation();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const currentUser = firebase.auth().currentUser;
+    if (currentUser) {
+      const db = firebase.firestore();
+      const userRef = db.collection('users').doc(currentUser.uid);
+
+      userRef.get().then((doc) => {
+        if (doc.exists) {
+          setUser(doc.data());
+        } else {
+          console.log('No such document!');
+        }
+      }).catch((error) => {
+        console.log('Error getting document:', error);
+      });
+    }
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      await firebase.auth().signOut();
+      navigation.navigate('Start');
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image
         style={styles.profileImg}
         source={require('../../assets/images/no-img.png')}
       />
-      <P>Voornaam Achternaam</P>
-
+      {user && (
+        <P>{`${user.firstName} ${user.lastName}`}</P>
+      )}
+      <Button onPress={handleLogout}>Log uit</Button>
     </View>
   );
 }
 
-export default Account
+export default Account;
 
 const styles = StyleSheet.create({
   container: {
