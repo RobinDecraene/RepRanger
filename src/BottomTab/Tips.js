01bb1850aecdeefd21f065ba2b85a7b570b608ff
decraenerobin <robin@esign.eu>
2024-05-03 12:11:36 +0200

    Update

--- src/BottomTab/Tips.js
+++ src/BottomTab/Tips.js
@@ -1,11 +1,14 @@
 import { StatusBar, StyleSheet, View } from 'react-native';
 import React from 'react';
 import { P } from '../../Components/Text';
+import { useNavigation } from '@react-navigation/native';
+import { Button } from '../../Components/Button';
 
 const Tips = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <P>Ga naar detail</P>
+      <Button onPress={() => navigation.navigate('Detail')}>Ga naar detail</Button>
       <StatusBar style="auto" />
     </View>
   );
