3bd93792a3e0d38506ce4596154cdfae88c502dd
decraenerobin <robin@esign.eu>
2024-05-23 15:48:02 +0200

    Update: update user data

--- src/Stack/EditAccount.js
+++ src/Stack/EditAccount.js
@@ -1,22 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { firebase } from '../../Firebase';
 
 import { Pressable, StyleSheet, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Title } from '../../Components/Title';
 import { Input } from '../../Components/Input';
 import { Button } from '../../Components/Button';
+import { ActivityIndicator } from 'react-native-paper';
 
 const EditAccount = () => {
   const navigation = useNavigation();
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const currentUser = firebase.auth().currentUser;
+        const userSnapshot = await firebase.firestore().collection('users').doc(currentUser.uid).get();
+        const userData = userSnapshot.data();
+        setFirstName(userData.firstName);
+        setLastName(userData.lastName);
+        setEmail(userData.email);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const handleSave = async () => {
+    const userRef = firebase.firestore().collection('users').doc(currentUser.uid);
+
+    const updatedData = {
+      firstName,
+      lastName,
+      email,
+    };
+
+    try {
+      await userRef.update(updatedData);
+      if (password) {
+        const user = firebase.auth().currentUser;
+        await user.updatePassword(password);
+      }
+      navigation.goBack();
+    } catch (error) {
+      console.error('Error updating user data:', error);
+    }
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#4E598C" />
+      </View>
+    );
+  }
+
   return (
     <ScrollView style={styles.base}>
       <View style={styles.container}>
-        <Pressable
-          onPress={() => navigation.goBack()}
-          style={styles.icon}
-        >
+        <Pressable onPress={() => navigation.goBack()} style={styles.icon}>
           <MaterialCommunityIcons name="arrow-left" color='#4E598C' size={30} />
         </Pressable>
 
@@ -27,31 +80,39 @@ const EditAccount = () => {
         />
 
         <Input
-          placeholder="Voornaam" 
+          placeholder={firstName}
+          value={firstName}
+          onChangeText={setFirstName}
         />
 
         <Input
-          placeholder="Achternaam" 
+          placeholder={lastName}
+          value={lastName}
+          onChangeText={setLastName}
         />
 
         <Input
-          placeholder="Email" 
+          placeholder={email}
+          value={email}
+          onChangeText={setEmail}
           keyboardType="email-address"
           autoCapitalize='none'
         />
 
         <Input
-          placeholder="Wachtwoord" 
+          placeholder="Wachtwoord"
+          value={password}
+          onChangeText={setPassword}
           secureTextEntry={true}
         />
 
-      <Button>Opslaan</Button>
+        <Button onPress={handleSave}>Opslaan</Button>
       </View>
     </ScrollView>
   );
-}
+};
 
-export default EditAccount
+export default EditAccount;
 
 const styles = StyleSheet.create({
   base: {
@@ -71,5 +132,10 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 55,
     left: 20
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
